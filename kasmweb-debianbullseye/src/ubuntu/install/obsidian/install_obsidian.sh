6491cb09e14ef380dd0cb1d85ff987f5c5f0befd
Jason Tan <8446265+asfritecx@users.noreply.github.com>
2024-04-13 10:15:02 +0800

    Update install_obsidian.sh
    update obsidian version

--- kasmweb-debianbullseye/src/ubuntu/install/obsidian/install_obsidian.sh
+++ kasmweb-debianbullseye/src/ubuntu/install/obsidian/install_obsidian.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -ex
 
-wget -q https://github.com/obsidianmd/obsidian-releases/releases/download/v1.5.3/obsidian_1.5.3_amd64.deb -O obsidian.deb
+wget -q https://github.com/obsidianmd/obsidian-releases/releases/download/v1.5.11/obsidian_1.5.11_amd64.deb -O obsidian.deb
 apt install -y ./obsidian.deb
 sed -i 's#/opt/Obsidian/obsidian#/opt/Obsidian/obsidian --no-sandbox##' /usr/share/applications/obsidian.desktop
 cp /usr/share/applications/obsidian.desktop $HOME/Desktop
